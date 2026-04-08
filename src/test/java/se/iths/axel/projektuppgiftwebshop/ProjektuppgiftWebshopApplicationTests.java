597450c06d8dd25ab5ba0f51d7e11a3974140ef8
Axel <axsty@home.home>
2026-04-08 11:42:50 +0200

    Initial commit

---
+++ src/test/java/se/iths/axel/projektuppgiftwebshop/ProjektuppgiftWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.axel.projektuppgiftwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektuppgiftWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
